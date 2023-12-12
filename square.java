8336e52dc82c1e4e20d486f3758c5c9c2ffeea43
Elliot Omark <elliot.omark@mrjaffesclass.com>
2023-12-12 11:27:56 -0800

    mmmmmm

---
+++ square.java
@@ -0,0 +1,51 @@
+
+/**
+ * Implement a circle
+ * 
+ * @author Mr. Jaffe 
+ * @version 2017-07-14
+ */
+public class square implements Shape
+{
+  private int sidelength;
+  private String color;
+
+  /**
+   * Constructor for objects of class square
+   */
+  public square(int length, String color)
+  {
+    this.sidelength = length;
+    this.color = color;
+  }
+
+  /**
+   * Get the area
+   * 
+   * @return     returns the area of the circle
+   */
+  public double getArea()
+  {
+    return this.sidelength * this.sidelength;
+  }
+
+  /**
+   * Get the perimeter
+   * 
+   * @return     returns the perimeter of the circle
+   */
+  public double getPerimeter()
+  {
+    return this.sidelength+this.sidelength+this.sidelength+this.sidelength;
+  }
+
+  /**
+   * Get the color
+   * 
+   * @return     returns the color of the circle
+   */
+  public String getColor()
+  {
+    return this.color;
+  }
+}
